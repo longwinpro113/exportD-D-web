b25b5c0769b1bdf77007967e03db1cc1bb2e2905
Long Nguyen <longwinpro113@gmail.com>
2026-04-03 15:16:11 +0700

    change api url

--- src/hooks/useFetchList.js
+++ src/hooks/useFetchList.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 
-// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// const API_URL = "http://localhost:5000";
 
 const useFetchList = (path, query, config = {}) => {
   const [data, setData] = useState([]);
